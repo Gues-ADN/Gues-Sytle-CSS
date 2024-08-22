cca0484262415708dc8d001fd966468e177745c4
Ankit Kumar <ankit.kumar@kankit.es>
2024-08-22 22:24:02 +0200

    Adding some main configuration and scroll where scrollbar is styles

---
+++ src/js/gsc.style.js
@@ -0,0 +1,11 @@
+const switchTheme = () => {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    if (currentTheme === 'dark') {
+      document.documentElement.setAttribute('data-theme', 'light');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    }
+  };
+  
+  document.querySelector('#theme-toggle').addEventListener('click', switchTheme);
+  
